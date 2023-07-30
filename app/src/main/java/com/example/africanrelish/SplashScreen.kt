3beb24ed3d66a86070c94482833ac146b3113e7c
j3rryl <jeremymunroe0@gmail.com>
2023-07-30 22:09:55 +0300

    Images

--- app/src/main/java/com/example/africanrelish/SplashScreen.kt
+++ app/src/main/java/com/example/africanrelish/SplashScreen.kt
@@ -1,11 +1,15 @@
 package com.example.africanrelish
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
 
 class SplashScreen : AppCompatActivity() {
     private val duration = 5800L
@@ -13,6 +17,12 @@ class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window: Window = this.window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.statusBarColor = ContextCompat.getColor(this, R.color.red)
+        }
         handler = Handler(Looper.getMainLooper())
         handler.postDelayed({
             val intent = Intent(this, MainActivity::class.java)
